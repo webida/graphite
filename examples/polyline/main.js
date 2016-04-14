88ab86f71712bbdd91ab58bdcb99e7380bc5e413
Shim Heungwoon <hw.shim@samsung.com>
2016-04-14 15:37:25 +0900

    [TASK] examples/polyline: simplified

--- examples/polyline/main.js
+++ examples/polyline/main.js
@@ -1,35 +1,17 @@
-document.getElementById('btnRun').addEventListener('click', function(oEvent) {
-    require(['graphite/graphite'], function(graphite) {
+require(['graphite/graphite'], function(graphite) {
 
-        var GraphiteShell = graphite.view.system.GraphiteShell;
-        var Color = graphite.base.Color;
-        var PointList = graphite.view.geometry.PointList;
-        var Rectangle = graphite.view.geometry.Rectangle;
-        var Polyline = graphite.view.widget.svg.Polyline;
-        var Svg = graphite.view.widget.svg.Svg;
+    var GraphiteShell = graphite.view.system.GraphiteShell;
+    var Polyline = graphite.view.widget.svg.Polyline;
+    var Svg = graphite.view.widget.svg.Svg;
 
-        var shell = new GraphiteShell('container');
-        var svg = new Svg();
-        shell.contents(svg);
+    var shell = new GraphiteShell('container');
+    var svg = new Svg();
+    shell.contents(svg);
 
-        var p1 = new Polyline();
-        p1.cursor('move')
-            .border(2, 'salmon')
-            .points(100,100,140,140,100,180);
-        svg.append(p1);
+    var p1 = new Polyline();
+    p1.cursor('move')
+        .border(2, 'salmon')
+        .points(100,100,140,140,100,180);
+    svg.append(p1);
 
-        console.log('------ end of main ------');
-    });
 });
-
-(function () {
-    var posInEl = function(oEvent) {
-        return {
-            x: oEvent.offsetX || oEvent.layerX,
-            y: oEvent.offsetY || oEvent.layerY
-        };
-    }; 
-    document.getElementById('mask').addEventListener('mousemove', function (ev) {
-        layerXY.textContent = posInEl(ev).x+', '+posInEl(ev).y;
-    });
-})();
