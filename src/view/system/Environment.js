3a8ed874de568169be51182f385135853bd402e8
Shim Heungwoon <hw.shim@samsung.com>
2016-04-14 15:18:20 +0900

    [TASK] Environment has been implemented
    This class supports getter, setter for saving environmental informations,
    such as whether current mode is debug-mode or not.

---
+++ src/view/system/Environment.js
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2012-2015 S-Core Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file Introduction
+ * @since 1.0.0
+ * @author hw.shim@samsung.com
+ */
+
+define([
+    'external/dom/dom',
+    'external/genetic/genetic',
+    'external/map/Map',
+    'graphite/base/Base'
+], function (
+    dom,
+    genetic,
+    Map,
+    Base
+) {
+    'use strict';
+
+    /**
+     * A Environment.
+     * @constructor
+     */
+    function Environment() {
+        Base.apply(this, arguments);
+        this.env = new Map();
+    }
+
+    genetic.inherits(Environment, Base, {
+
+        /**
+         * @param {object} key
+         * @param {object} value
+         */
+        set: function (key, value) {
+            this.env.set(key, value);
+            return this;
+        },
+
+        /**
+         * @param {object} key
+         */
+        get: function (key) {
+            return this.env.get(key);
+        }
+    });
+
+    Environment.global = new Environment();
+
+    Environment.setLocationHash = function () {
+        var p, g = Environment.global;
+        var hash = window.location.hash.slice(1);
+        var tokens = hash.split('&');
+        tokens.forEach(function (token) {
+            p = token.split('=');
+            g.set(p[0], p[1]);
+        });
+    };
+
+    Environment.loadDebugMode = function (shell) {
+        require(['graphite/view/system/debug/DebugTools'], function (DebugTools) {
+            DebugTools.load(shell);
+        });
+    }
+
+    return Environment;
+});
