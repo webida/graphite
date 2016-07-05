8544bba46c33de975791c001d923396d06c025d8
Shim Heungwoon <hw.shim@samsung.com>
2016-07-05 21:24:39 +0900

    [TASK] Cursor has been implemented

---
+++ src/view/resource/Cursor.js
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2012-2016 S-Core Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"),
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @file Mouse Cursor Constants
+ * @since 1.0.0
+ * @author hw.shim@samsung.com
+ */
+
+define([
+    '../geometry/Position',
+], function (
+    Position
+) {
+    'use strict';
+
+    /**
+     * Mouse Cursor Constants.
+     */
+    var Cursor = {};
+
+    Cursor[Position.EAST] = 'e-resize';
+    Cursor[Position.WEST] = 'w-resize';
+    Cursor[Position.SOUTH] = 's-resize';
+    Cursor[Position.NORTH] = 'n-resize';
+    Cursor[Position.NORTH_EAST] = 'ne-resize';
+    Cursor[Position.NORTH_WEST] = 'nw-resize';
+    Cursor[Position.SOUTH_EAST] = 'se-resize';
+    Cursor[Position.SOUTH_WEST] = 'sw-resize';
+
+    return Cursor;
+});
